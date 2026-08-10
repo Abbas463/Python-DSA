d2a28c47355416b8b0571877d5e871ea968c624d
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-10 18:09:51 +0430

    ADD items

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # fruits = ["apple", "banana", "cherry"]
 # print(fruits)
 
-fruits = ["apple", "banana", "cherry"]
-print(fruits[0])    # First item
-print(fruits[-1])   # Last item
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[0])    # First item
+# print(fruits[-1])   # Last item
+
+fruits = ["apple", "banana"]
+fruits.append("cherry")    # Add to end
+fruits.insert(1, "mango")  # Add at position 1
+print(fruits)
