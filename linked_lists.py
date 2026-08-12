b2775f6a836f2c787a20c2d21bab58380bf732c2
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-12 09:41:56 +0430

    TRAVERSE (print all elements)

--- linked_lists.py
+++ linked_lists.py
@@ -3,15 +3,30 @@ class Node:
         self.data = data
         self.next = None
 
-# Create nodes
+# # Create nodes
+# node1 = Node(10)
+# node2 = Node(20)
+# node3 = Node(30)
+# # Link them
+# node1.next = node2
+# node2.next = node3
+# # Head points to first node
+# head = node1
+# print(head.data)  # 10
+
+
+def print_list(head):
+    current = head
+    while current is not None:
+        print(current.data, end=" -> ")
+        current = current.next
+    print("None")
+
+# Create list: 10 -> 20 -> 30
 node1 = Node(10)
 node2 = Node(20)
 node3 = Node(30)
-
-# Link them
 node1.next = node2
 node2.next = node3
 
-# Head points to first node
-head = node1
-print(head.data)  # 10
+print_list(node1)  # 10 -> 20 -> 30 -> None
