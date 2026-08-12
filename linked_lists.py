3f6360c85d7d5ff8aeff967da483c577388beb91
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-12 09:40:41 +0430

    CREATE a linked list

--- linked_lists.py
+++ linked_lists.py
@@ -3,3 +3,15 @@ class Node:
         self.data = data
         self.next = None
 
+# Create nodes
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+# Link them
+node1.next = node2
+node2.next = node3
+
+# Head points to first node
+head = node1
+print(head.data)  # 10
