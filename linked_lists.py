a716e370fbacc6e3afef42891078e8a6b83c81bb
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-13 08:37:46 +0430

    CREATE 3 nodes linked together

--- linked_lists.py
+++ linked_lists.py
@@ -3,30 +3,9 @@ class Node:
         self.data = data
         self.next = None
 
-# # Create nodes
-# node1 = Node(10)
-# node2 = Node(20)
-# node3 = Node(30)
-# # Link them
-# node1.next = node2
-# node2.next = node3
-# # Head points to first node
-# head = node1
-# print(head.data)  # 10
-
-
-def print_list(head):
-    current = head
-    while current is not None:
-        print(current.data, end=" -> ")
-        current = current.next
-    print("None")
-
-# Create list: 10 -> 20 -> 30
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-node1.next = node2
-node2.next = node3
-
-print_list(node1)  # 10 -> 20 -> 30 -> None
+a = Node(10)
+b = Node(20)
+c = Node(30)
+a.next = b
+b.next = c
+head = a
